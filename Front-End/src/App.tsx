628cdca5ce5b28df9a50c3ad4368fea00342761a
David Adams <budavidadams@gmail.com>
2023-03-02 01:21:07 +0000

    Feature: Bubble background (with light & dark)

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { ThemeToggleButton } from "./components/ThemeToggleButton";
+import { HomePage } from "./pages/home-page/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,12 @@ function App() {
 
   return (
     <div className="App" data-theme={!isLightMode ? "dark" : "light"}>
+      <div>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="title"></div>
+      </div>
       <div className="header">
         <div className="align-row">
           <img src={reactLogo} className="logo" alt="logo" />
@@ -20,6 +27,10 @@ function App() {
             setIsLightMode={setIsLightMode}
           />
         </div>
+      </div>
+      <div className="card">
+        {/*pages*/}
+        <HomePage />
       </div>
     </div>
   );
---
+++ Front-End/src/pages/home-page/HomePage.tsx
@@ -0,0 +1,16 @@
+import "./HomePage.css";
+
+type HomePageProps = {};
+
+export const HomePage = ({}: HomePageProps) => {
+  return (
+    <div className="container">
+      <h1>Find your perfect tech stack in 60 seconds...</h1>
+      {/* <button className="buttonContent">
+        <div className="left"></div>
+        Get Started!
+        <div className="right"></div>
+      </button> */}
+    </div>
+  );
+};
