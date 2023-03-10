49ea4c36722e5cc8235222f595ae0b020556cba9
David Adams <budavidadams@gmail.com>
2023-03-10 22:28:22 +0000

    Feature: Finish Home Page

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -5,35 +5,34 @@ import { ThemeToggleButton } from "./components/ThemeToggleButton";
 import { HomePage } from "./pages/home-page/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [isLightMode, setIsLightMode] = useState(false);
+    const [isLightMode, setIsLightMode] = useState(false);
 
-  return (
-    <div className="App" data-theme={!isLightMode ? "dark" : "light"}>
-      <div>
-        <div id="stars"></div>
-        <div id="stars2"></div>
-        <div id="stars3"></div>
-        <div id="title"></div>
-      </div>
-      <div className="header">
-        <div className="align-row">
-          <img src={reactLogo} className="logo" alt="logo" />
-          <h1>WhichStack?</h1>
+    return (
+        <div className="App" data-theme={!isLightMode ? "dark" : "light"}>
+            <div>
+                <div id="stars"></div>
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+                <div id="title"></div>
+            </div>
+            <div className="header">
+                <div className="align-row">
+                    <img src={reactLogo} className="logo" alt="logo" />
+                    <h1>WhichStack?</h1>
+                </div>
+                <div className="card">
+                    <ThemeToggleButton
+                        isLightMode={isLightMode}
+                        setIsLightMode={setIsLightMode}
+                    />
+                </div>
+            </div>
+            <div className="card">
+                {/*pages*/}
+                <HomePage />
+            </div>
         </div>
-        <div className="card">
-          <ThemeToggleButton
-            isLightMode={isLightMode}
-            setIsLightMode={setIsLightMode}
-          />
-        </div>
-      </div>
-      <div className="card">
-        {/*pages*/}
-        <HomePage />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- Front-End/src/pages/home-page/HomePage.tsx
+++ Front-End/src/pages/home-page/HomePage.tsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
+import { PopularStacks, Stack } from "../../assets/PopularStacks";
 import "./HomePage.css";
 
 type HomePageProps = {};
 
 export const HomePage = ({}: HomePageProps) => {
-  return (
-    <div className="container">
-      <h1>Find your perfect tech stack in 60 seconds...</h1>
-      {/* <button className="buttonContent">
-        <div className="left"></div>
-        Get Started!
-        <div className="right"></div>
-      </button> */}
-    </div>
-  );
+    const [stacks, setStacks] = useState<Stack[]>(PopularStacks);
+    return (
+        <div className="container">
+            <h1>Find your perfect tech stack in 60 seconds...</h1>
+            <GoButton />
+            <br />
+            <hr />
+            <h3>Popular Tech Stacks</h3>
+            <div className="stack-container">
+                {stacks.map((stack) => (
+                    <TechStackCard stack={stack} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const GoButton = () => {
+    return (
+        <button className="button-81" role="button">
+            Get Started
+        </button>
+    );
+};
+
+type TechStackCardProps = {
+    stack: Stack;
+};
+const TechStackCard = ({ stack }: TechStackCardProps) => {
+    return (
+        <div className="techstack-card">
+            <img className="stack-image" src={stack.image} alt={stack.name} />
+            <h3 style={{ margin: 3 }}>{stack.name}</h3>
+            <p style={{ fontSize: 12 }}>{stack.description}</p>
+        </div>
+    );
 };
