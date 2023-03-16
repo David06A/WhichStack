afc330703ae06dd0409ee226f0a90ee6385d3f1f
shivster <shivster2401@gmail.com>
2023-03-16 22:10:57 +0000

    An example request to the stack chooser

---
+++ Back-End/example.py
@@ -0,0 +1,41 @@
+import asyncio
+import json
+
+import asyncio
+
+async def example():
+    async with aiohttp.ClientSession() as session:
+        payload = {
+            "user_context": {
+                "purpose": [
+                    "2"
+                ],
+                "experience": [
+                    "1"
+                ],
+                "budget": [
+                    "1"
+                ],
+                "timeline": [
+                    "1"
+                ],
+                "stacks": [
+                    "4", "5"
+                ],
+                "languages": [
+                    "4"
+                ],
+                "database": [
+                    "4"
+                ],
+                "providers": [
+                    "4"
+                ]
+            }
+        }
+
+        async with session.post('http://127.0.0.1:42069/stack/chooser', json = payload) as response:
+            print(await response.json())
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(example())
